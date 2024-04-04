53d430ba2bc945660d4739b2fa22304c718caaeb
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-04 11:39:13 +0545

    mini hoisting

--- jsfunctions/scope.js
+++ jsfunctions/scope.js
@@ -1,25 +1,25 @@
 //{} this is scope
-// if(true){
-//     let a=10
-//     const b=20
-//     var c=30
-// }
+if(true){
+    let a=10
+    const b=20
+    var c=30
+}
 
 
 // console.log(c)
 // console.log(a)
 // console.log(b)
 //var is global scope which may cause error when importing as it might change the var varriable
-// function one(){
-//     const username= "hitesh"
-//     function two(){
-//         const website="youtube"
-//         console.log(username);
-//     }
-//     console.log(website);
-//     two()
-// }
-// one()
+function one(){
+    const username= "hitesh"
+    function two(){
+        const website="youtube"
+      //  console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
 //in above example we can learn that the nested fucntions or loops the 
 // parents class can't use the childrens cvarriables
 //but the children can run it'
@@ -27,8 +27,18 @@ if (true){
     const fn="ram"
     if(fn==="ram"){
         const web ="youtube"
-        console.log(web);
+       // console.log(web);
     }
-    console.log(web);
+   // console.log(web);
 }
-console.log(username);
+// console.log(username);
+//intresting//
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+console.log(addTwo(5))
+//if declared like below doesnpt wrok due to histing
+const addTwo= function(num){
+    return num+2
+}
