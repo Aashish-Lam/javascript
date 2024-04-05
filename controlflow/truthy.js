78aa3cb879845b7103d0d9154de5ba7df432458e
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-05 22:11:32 +0545

    erererr

--- controlflow/truthy.js
+++ controlflow/truthy.js
@@ -1,10 +1,27 @@
 const userEmail ="hasas"
-if (userEmail){
-    console.log("got username")
-}
-else {
-    console.log("no user")
-}
+// if (userEmail){
+//     console.log("got username")
+// }
+// else {
+//     console.log("no user")
+// }
 // falsy values 0,-0,0n ,"",null ,undefined ,NaN
 // true values '0' anything inside string is truthy value
 //"0","false " " []
+// if(userEmail.length !==0){
+//     console.log("notgood")
+// }
+// const emptyObject={}
+// if (Object.keys(emptyObject).length===0){
+//     console.log('object is empty')
+// }
+//nullish caolescing
+// let val1;
+// val1= null ?? 10
+// console.log(`${val1}`)
+
+// val1=undefined ?? 15
+// console.log(`${val1}`)
+const icetea=500
+icetea>=90 ? console.log("less than dothers"):
+console.log("more than 80");
