88b69542b273aad7a93be4697ea1e9f9ea2e27aa
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-07 03:32:54 +0545

    changed the folder

--- arraysorting.js
+++
@@ -1,9 +0,0 @@
-let a= [22,4,5,6,7,11,55,3,33]
-function compare(c,b){
-    return c-b
-}
-console.log(a.sort(function compare(c,b){
-    return c-b
-}));
-console.log(a)
-// function sorted(c,b);=> c-b
--- operations.js
+++
@@ -1,8 +0,0 @@
-let value =3
-let negValue = -value
-console.log(2+3);
-console.log(2**2);
-let str1 ="hello"
-let str2=" ash"
-let str3= str1+str2;
-console.log(str3)  
