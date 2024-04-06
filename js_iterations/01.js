04a8455b7fee50221ed771c773d4253cd3928560
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-06 22:29:01 +0545

    iterable part 2

--- js_iterations/01.js
+++ js_iterations/01.js
@@ -10,10 +10,28 @@
 //     }
 //     console.log(element)
 // }
-for(i=0; i < 10;i++){
-    console.log(`outer loop :${i}`);
-    for(let j=0; j<=10; j++){
-       // console.log(`inner lopp ${j} and inner loop ${i}`)
-       console.log(i+'*'+j +'='+ i*j)
-    }
+// for(i=0; i < 10;i++){
+//     console.log(`outer loop :${i}`);
+//     for(let j=0; j<=10; j++){
+//        // console.log(`inner lopp ${j} and inner loop ${i}`)
+//        console.log(i+'*'+j +'='+ i*j)
+//     }
+// }
+let myarray= ["flash","batman","super"]
+for (let i= 0; i< myarray.length; i++){
+    const Element= myarray[i];
+    console.log(Element);
 }
+
+//breakznd continuw
+for (let i = 0; i<=20; i++){
+    if(i==5){
+        console.log("lopp")
+        break;
+    } 
+}
+//     default
+//         console.log("default case")
+//         break;
+    
+// }
