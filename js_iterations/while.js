30aeeea50c17217c1688e87952d9ae7f40aaaff0
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-07 13:36:18 +0545

    css flex and while loops

---
+++ js_iterations/while.js
@@ -0,0 +1,17 @@
+let i=0
+while(i<=5){
+    console.log(`value of index is${i}`)
+    i= i+1;
+}
+let myarray=[1,2,5,65]
+let j=0;
+while(j<myarray.length){
+    console.log(myarray[j]);
+    j= j+ 1;
+}
+let score =11
+do{
+    console.log(`score is ${score}`);
+    score++
+} while(score<=10)
+// cls
