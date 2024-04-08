9d81c81dbfaf1bfb3577e366e281bc1b9c82fd17
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-08 19:25:52 +0545

    git manipulations

--- DOM/01.js
+++ DOM/01.js
@@ -1,2 +1,22 @@
-let a =document.getElementById('title').getAttribute('id')
-console.log(a);
+document.getElementById('title').getAttribute('id')
+title.style.backgroundColor="purple"
+title.style.borderRadius="20px"
+title.innerHTML
+title.textContent
+title.innerText
+// 
+const tab = document.querySelector('ul')
+let a= tab.querySelector('li')
+a.textContent="five"
+let tempList= document.querySelectorAll('li')
+tempList[0].style.background='red'
+tempList.forEach((li)=>{
+    li.style.backgroundColor='green'
+}
+
+)
+let cn=document.getElementsByClassName('Listclass')
+let mycn =Array.from(cn)
+mycn.forEach((li)=>{
+    li.style.color='red'
+})
