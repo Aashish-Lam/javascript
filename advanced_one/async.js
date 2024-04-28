ef63283a8c7291a8a679f9c9a73a94bf7efa09e5
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-28 21:58:20 +0545

    continued

--- advanced_one/async.js
+++ advanced_one/async.js
@@ -1,6 +1,13 @@
-async function random(){
-   let raw= await fetch('https://randomuser.me/api/')
-   let ans =await raw.json()
-   console.log(ans)
-}
-random()
+// async function random(){
+//    let raw= await fetch('https://randomuser.me/api/')
+//    let ans =await raw.json()
+//    console.log(ans)
+// }
+// random()
+async function myFunction() {
+   return "Hello";
+ }
+ myFunction().then(
+   function(value) {myDisplayer(value);},
+   function(error) {myDisplayer(error);}
+ );
